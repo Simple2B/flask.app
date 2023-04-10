601f7ca79f421e0e837881e49598ddd10d7a3248
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-04-10 15:04:26 +0300

    work

--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,11 @@
 from flask import render_template, Blueprint
+from flask_login import login_required
+
 
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
+@login_required
 def index():
     return render_template("index.html")
