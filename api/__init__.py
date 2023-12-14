f6cc58e9d6a22590e43b543ee6d0df54993f7d97
Taraslysak <lysak.ipr@gmail.com>
2023-12-14 16:23:24 +0200

    Add custom_generate_unique_id function to FastAPI initialization

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+
 from config import config
+
+from .utils import custom_generate_unique_id
 from .routes import router
 
 CFG = config()
 
-app = FastAPI(version=CFG.VERSION)
+app = FastAPI(
+    version=CFG.VERSION, generate_unique_id_function=custom_generate_unique_id
+)
 app.include_router(router)
 
 
-@app.get("/")
+@app.get("/", tags=["root"])
 async def root():
     return RedirectResponse(url="/docs")
